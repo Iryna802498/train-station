e9b0a140f2b65fdb9aa1565d0f272cbd2734d282
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-03 14:25:50 +0200

    Add Station model

--- train/models.py
+++ train/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField(
+        validators=[MinValueValidator(-90), MaxValueValidator(90)]
+    )
+    longitude = models.FloatField(
+        validators=[
+            MinValueValidator(-180),
+            MaxValueValidator(180)
+        ]
+    )
