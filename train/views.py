a2a4c2831a3a1a51db7332701d84f362b4c0c807
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-03 19:07:55 +0200

    Add StationViewSet for Station model

--- train/views.py
+++ train/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
+from .models import Station
+from .serializers import StationSerializer
+from .permissions import IsAdminOrIfAuthenticatedReadOnly
 
-# Create your views here.
+
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
