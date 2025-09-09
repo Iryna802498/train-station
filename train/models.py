584ceeb5f330125858e15b438f3a7efdd47daa67
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-09 13:13:28 +0200

    Add TrainType model

--- train/models.py
+++ train/models.py
@@ -1,5 +1,8 @@
+import os
+import uuid
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
 
 
 class Station(models.Model):
@@ -13,3 +16,28 @@ class Station(models.Model):
             MaxValueValidator(180)
         ]
     )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+def train_type_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/trains/", filename)
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(
+        null=True,
+        upload_to=train_type_image_file_path
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
