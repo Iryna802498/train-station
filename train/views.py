e57549f0aec031990a0d35922364a0033a6619d4
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 13:42:03 +0200

    Add custom endpoint for upload TrainType image

--- train/views.py
+++ train/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import GenericViewSet
 from .models import Station, TrainType, Crew, Route
 from .serializers import (
     StationSerializer,
-    TrainTypeSerializers,
+    TrainTypeSerializer,
+    TrainTypeImageSerializer,
     CrewSerializer,
     RouteSerializer,
     RouteListSerializer,
@@ -25,11 +28,34 @@ class StationViewSet(
 class TrainTypeViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
     GenericViewSet
 ):
     queryset = TrainType.objects.all()
-    serializer_class = TrainTypeSerializers
+    serializer_class = TrainTypeSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "upload_image":
+            return TrainTypeImageSerializer
+        return TrainTypeSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific train type"""
+        train_type = self.get_object()
+        serializer = self.get_serializer(train_type, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CrewViewSet(
