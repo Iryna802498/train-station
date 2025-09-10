9867de1ae607b47392263adb96d43cdc94764c0b
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 12:16:41 +0200

    Add path for CrewViewSet

--- train/urls.py
+++ train/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import StationViewSet, TrainTypeViewSet
+from .views import StationViewSet, TrainTypeViewSet, CrewViewSet
 
 
 router = routers.DefaultRouter()
 router.register("stations", StationViewSet, basename="station")
 router.register("train-types", TrainTypeViewSet, basename="train-type")
+router.register("crews", CrewViewSet, basename="crew")
 urlpatterns = [
     path("", include(router.urls)),
 ]
