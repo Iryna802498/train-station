b1dd909e14ada6548ab4d4b98615574fef7a57b3
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 13:49:56 +0200

    Add user app URLs for registration, management, and JWT authentication

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from user.views import CreateUserView, ManageUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+app_name = "user"
+
+urlpatterns = [
+    path("me/", ManageUserView.as_view(), name="manage"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
