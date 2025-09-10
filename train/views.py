12b2f17664dc901a1df2d26effdc808520eda552
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 13:08:42 +0200

    Add RouteViewSet for Route model

--- train/views.py
+++ train/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
-from .models import Station, TrainType, Crew
-from .serializers import StationSerializer, TrainTypeSerializers, CrewSerializer
+from .models import Station, TrainType, Crew, Route
+from .serializers import (
+    StationSerializer,
+    TrainTypeSerializers,
+    CrewSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer
+)
 from .permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
@@ -33,3 +40,21 @@ class CrewViewSet(
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet
+):
+    queryset = Route.objects.select_related("source", "destination")
+    serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return RouteSerializer
