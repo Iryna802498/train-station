83b057200646cf66af73f72e6b45d6cd8df1e35f
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 12:13:37 +0200

    Add Crew to admin panel

--- train/admin.py
+++ train/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Station, TrainType
+from .models import Station, TrainType, Crew
 
 
 admin.site.register(Station)
 admin.site.register(TrainType)
+admin.site.register(Crew)
