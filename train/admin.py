09ca65d8f158444054418b07a1ed93b05f626dc0
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 19:03:34 +0200

    Add Train model to admin panel

--- train/admin.py
+++ train/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Station, TrainType, Crew, Route
+from .models import (
+    Station,
+    TrainType,
+    Crew,
+    Route,
+    Train
+)
 
 
 admin.site.register(Station)
 admin.site.register(TrainType)
 admin.site.register(Crew)
 admin.site.register(Route)
+admin.site.register(Train)
