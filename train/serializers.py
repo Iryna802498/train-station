3fe5761ab476764190fde5c65a55cb985a764848
Iryna Mishchenko <djrobentab@gmail.com>
2025-09-10 13:06:43 +0200

    Add list and detail serializers for Route model

--- train/serializers.py
+++ train/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Station, TrainType, Crew
+from .models import Station, TrainType, Crew, Route
 
 
 class StationSerializer(serializers.ModelSerializer):
@@ -35,4 +35,50 @@ class CrewSerializer(serializers.ModelSerializer):
             "last_name",
             "full_name",
             "position"
+        )
+
+
+class RouteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Route
+        fields = (
+            "id",
+            "source",
+            "destination",
+            "distance"
+        )
+
+
+class RouteListSerializer(RouteSerializer):
+    source = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="name"
+    )
+    destination = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="name"
+    )
+
+    class Meta:
+        model = Route
+        fields = (
+            "id",
+            "source",
+            "destination",
+            "distance"
+        )
+
+
+class RouteDetailSerializer(RouteSerializer):
+    source = StationSerializer(read_only=True)
+    destination = StationSerializer(read_only=True)
+
+    class Meta:
+        model = Route
+        fields = (
+            "id",
+            "source",
+            "destination",
+            "distance"
         )
